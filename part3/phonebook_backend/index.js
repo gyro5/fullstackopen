d8d43816b3a8a686c719d1a1d31e68eb1c96cd71
gyro <109408760+gyro5@users.noreply.github.com>
2025-07-03 23:32:34 +1000

    done 3.3

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+
+const persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+const app = express()
+
+app.get('/info', (req, res) => {
+    res.send(
+        `<div>Phonebook has info for ${persons.length} people</div>
+        <div>${new Date(Date.now()).toString()}</div>`
+    )
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    }
+    else {
+        res.status(404).json({error: "Not found"})
+    }
+})
+
+// Run the app
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`App running at http://localhost:${PORT}/api/persons`)
+})
